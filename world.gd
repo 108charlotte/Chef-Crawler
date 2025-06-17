63c0719d2c0b0871c2b7f27c9c2ed47da1144ec3
108charlotte <108sarahsmith@gmail.com>
2025-06-17 07:00:45 -0400

    Floor tiles successfully populating entire viewport

--- world.gd
+++ world.gd
@@ -1,13 +1,13 @@
-extends Control
+extends Node2D
 
-const TILE_SIZE = 32
+const TILE_SIZE = 16
 
 const LEVEL_SIZES = [
 	Vector2(30, 30), 
 	Vector2(35, 35),
 	Vector2(40, 40),  
 	Vector2(45, 45),
-	Vector2(50,50)
+	Vector2(50, 50)
 ]
 
 const LEVEL_ROOM_COUNTS = [5, 7, 9, 12, 15]
@@ -34,16 +34,25 @@ var player_tile
 var score = 0
 
 func _ready(): 
-	DisplayServer.window_set_size(Vector2(1280, 720))
 	randomize()
 	build_level()
-	
+
+	# Place player at center of tilemap grid
+	var map_center = level_size / 2
+	var player_pos = tile_map.map_to_local(Vector2i(map_center)) + Vector2(TILE_SIZE / 2, TILE_SIZE / 2)
+	player.position = player_pos
+
 func build_level(): 
 	rooms.clear()
 	map.clear()
 	tile_map.clear()
 	
-	level_size = LEVEL_SIZES[level_num]
+	var screen_size = get_viewport_rect().size
+	var tiles_x = ceil(screen_size.x / TILE_SIZE)
+	var tiles_y = ceil(screen_size.y / TILE_SIZE)
+
+	level_size = Vector2(tiles_x, tiles_y)
+
 	for x in range(level_size.x): 
 		map.append([])
 		for y in range(level_size.y): 
