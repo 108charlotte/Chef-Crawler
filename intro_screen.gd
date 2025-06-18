2883e8acb2f780f8ab4c9b0a7b4e0a95616e26d0
108charlotte <108sarahsmith@gmail.com>
2025-06-18 11:05:56 -0400

    Trying to debug start button not working when pressed, writing function so I can know what is intercepting the input

--- intro_screen.gd
+++ intro_screen.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var game = get_parent()
 @onready var world = game.get_node("World")
 @onready var stats = game.get_node("Stats")
-@onready var start_button = $StartButton
+@onready var start_button = game.get_node("TitleScreen2/TitleScreenCamera/GameTitle/StartButton")
 @onready var camera = game.get_node("World/Player/Camera2D")
 @onready var title_camera = $TitleScreenCamera
 
@@ -12,10 +12,18 @@ func _ready():
 	stats.visible = false
 	title_camera.make_current()
 	start_button.pressed.connect(_on_startButton_pressed)
+	print("StartButton is: ", start_button)
+	# start_button.emit_signal("pressed")
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		print("Mouse button pressed at: ", event.position)
+		print("Mouse button pressed at: ", event.position)
+		var hovered = get_viewport().gui_get_mouse_focus()
+		if hovered != null:
+			print("Hovered UI node: ", hovered.name)
+		else:
+			print("No UI Control node hovered at this position.")
 
 func _on_startButton_pressed() -> void:
 	print("Start button pressed!")
