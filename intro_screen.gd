f5956e7626b7ceb0542efbe8de2245435e6be627
108charlotte <108sarahsmith@gmail.com>
2025-06-20 12:44:17 -0400

    Got start button working, polished intro screens

--- intro_screen.gd
+++ intro_screen.gd
@@ -1,13 +1,13 @@
 extends Control
 
-@onready var start_button = $StartButton
+@onready var start_button = $Button
 @onready var title_camera = $TitleScreenCamera
 
 func _ready():
 	title_camera.make_current()
-	print("StartButton is: ", start_button)
-	_on_start_button_pressed()
+	# _on_start_button_pressed()
 
-func _on_start_button_pressed() -> void:
+
+func _on_button_pressed() -> void:
 	print("Start button pressed!")
 	get_tree().change_scene_to_file("res://credits.tscn")
